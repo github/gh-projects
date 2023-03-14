655abc3fd0ac442c5e95623a14f2b71c9b587575
Ariel Deitcher <mntlty@github.com>
2023-03-14 08:15:52 -0700

    integration tests for user

--- integration_tests.sh
+++ integration_tests.sh
@@ -5,7 +5,8 @@
 
 # required environment variables:
 # - ORG_NAME: the name of the organization to use for testing for which you are an admin.
-# - ITEM_URL: the URL of an issue or pull request in the organization to add to a project owned by ORG_NAME
+# - USER_NAME: the name of the user to use for testing .
+# - ITEM_URL: the URL of an issue or pull request in the organization to add to a project owned by ORG_NAME and USER_NAME
 
 set -ex
 
@@ -14,8 +15,15 @@ if [[ -z $ORG_NAME ]]; then
     exit 1
 fi
 
+
+if [[ -z $USER_NAME ]]; then
+    echo "USER_NAME must be set"
+    exit 1
+fi
+
 go build -o gh-projects projects.go
 
+## org tests
 PROJECT_NUMBER=$(./gh-projects create --org $ORG_NAME --title clitest --format=json | jq '.number')
 ./gh-projects view $PROJECT_NUMBER --org $ORG_NAME --format=json  | jq .
 
@@ -39,3 +47,28 @@ ITEM_ID=$(./gh-projects item-create $PROJECT_NUMBER --org $ORG_NAME --title 'dra
 ./gh-projects item-archive $PROJECT_NUMBER --org $ORG_NAME --id $ITEM_ID --format=json | jq .
 ./gh-projects item-delete $PROJECT_NUMBER --id $ITEM_ID --org $ORG_NAME --format=json  | jq .
 ./gh-projects delete $PROJECT_NUMBER --org $ORG_NAME --format=json | jq .
+
+## user tests
+PROJECT_NUMBER=$(./gh-projects create --user $USER_NAME --title clitest --format=json | jq '.number')
+./gh-projects view $PROJECT_NUMBER --user $USER_NAME --format=json  | jq .
+
+./gh-projects list --user $USER_NAME --format=json  | jq .
+
+COPY_PROJECT_NUMBER=$(./gh-projects copy $PROJECT_NUMBER --source-user $USER_NAME --target-user $USER_NAME --title new-copy --format=json | jq '.number')
+./gh-projects delete $COPY_PROJECT_NUMBER --user $USER_NAME --format=json  | jq .
+
+./gh-projects edit $PROJECT_NUMBER --user $USER_NAME --title edited-clitest --format=json  | jq .
+./gh-projects field-list $PROJECT_NUMBER --user $USER_NAME --format=json  | jq .
+FIELD_ID=$(./gh-projects field-create $PROJECT_NUMBER --user $USER_NAME --data-type TEXT --name custom-text --format=json | jq '.id')
+./gh-projects field-delete --id $FIELD_ID --format=json | jq .
+
+if [[ -n $ITEM_URL ]]; then
+    ./gh-projects item-add $PROJECT_NUMBER --user $USER_NAME --url $ITEM_URL --format=json | jq .
+fi
+
+ITEM_ID=$(./gh-projects item-create $PROJECT_NUMBER --user $USER_NAME --title 'draft issue' --format=json | jq '.id')
+./gh-projects item-list $PROJECT_NUMBER --user $USER_NAME --format=json | jq .
+
+./gh-projects item-archive $PROJECT_NUMBER --user $USER_NAME --id $ITEM_ID --format=json | jq .
+./gh-projects item-delete $PROJECT_NUMBER --id $ITEM_ID --user $USER_NAME --format=json  | jq .
+./gh-projects delete $PROJECT_NUMBER --user $USER_NAME --format=json | jq .
