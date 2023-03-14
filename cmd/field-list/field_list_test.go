eeeb389d8b770c2cdd1d6c079fd2cc2d0c74907f
Ariel Deitcher <mntlty@github.com>
2023-03-13 17:26:51 -0700

    fix tests

--- cmd/field-list/field_list_test.go
+++ cmd/field-list/field_list_test.go
@@ -42,6 +42,8 @@ func TestRunList_User(t *testing.T) {
 			"variables": map[string]interface{}{
 				"login":  "monalisa",
 				"number": 1,
+				"first":  100,
+				"after":  nil,
 			},
 		}).
 		Reply(200).
@@ -124,6 +126,8 @@ func TestRunList_Org(t *testing.T) {
 			"variables": map[string]interface{}{
 				"login":  "github",
 				"number": 1,
+				"first":  100,
+				"after":  nil,
 			},
 		}).
 		Reply(200).
@@ -203,6 +207,8 @@ func TestRunList_Me(t *testing.T) {
 			"query": "query ViewerProjectWithFields.*",
 			"variables": map[string]interface{}{
 				"number": 1,
+				"first":  100,
+				"after":  nil,
 			},
 		}).
 		Reply(200).
@@ -282,6 +288,8 @@ func TestRunList_Empty(t *testing.T) {
 			"query": "query ViewerProjectWithFields.*",
 			"variables": map[string]interface{}{
 				"number": 1,
+				"first":  100,
+				"after":  nil,
 			},
 		}).
 		Reply(200).
