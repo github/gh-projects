4e7c3fd1cb006d3d58db0c05042fc3bba7f075dc
Ariel Deitcher <mntlty@github.com>
2023-03-17 18:15:55 -0700

    field-list limit option

--- cmd/field-list/field_list.go
+++ cmd/field-list/field_list.go
@@ -88,6 +88,7 @@ gh projects field-list 1 --org github --limit 30
 	listCmd.Flags().StringVar(&opts.userOwner, "user", "", "Login of the user owner. Use \"@me\" for the current user.")
 	listCmd.Flags().StringVar(&opts.orgOwner, "org", "", "Login of the organization owner.")
 	listCmd.Flags().StringVar(&opts.format, "format", "", "Output format, must be 'json'.")
+	listCmd.Flags().StringVar(&opts.limit, "limit", "", "Maximum number of fields. Defaults to 100. Set to 'none' to list all fields.")
 
 	// owner can be a user or an org
 	listCmd.MarkFlagsMutuallyExclusive("user", "org")
